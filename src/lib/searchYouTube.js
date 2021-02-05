b51e0d81b72135c43873c979c0305ff58eadaeac
Colton Medlin <coltonmedlin@gmail.com>
2021-02-05 15:28:59 -0800

    eod 2/5

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,22 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  $.ajax({
+    url: `https://www.googleapis.com/youtube/v3/search?key=${ytKey}`,
+    type: 'GET',
+    options: {
+      query: options,
+      max: 5,
+      key: ytKey
+    },
+    contentType: 'application/json',
+    success: callback,
+    error: function(error) {
+      console.error('yt Failed', error);
+    }
+  });
+
 };
+
+searchYouTube('funny cats', () => console.log('success'));
 
 export default searchYouTube;
